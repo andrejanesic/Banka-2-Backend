e35ed5952c3cab13bcaeee8696df8506bb3d3a8d
Andreja Nesic <office@andrejanesic.com>
2023-05-15 20:11:06 +0200

    Added caching, redis

--- main/src/main/java/rs/edu/raf/si/bank2/main/MainApplication.java
+++ main/src/main/java/rs/edu/raf/si/bank2/main/MainApplication.java
@@ -2,10 +2,12 @@ package rs.edu.raf.si.bank2.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableCaching
 @EnableJpaRepositories("rs.edu.raf.si.bank2.main.repositories.mariadb")
 @EnableMongoRepositories("rs.edu.raf.si.bank2.main.repositories.mongodb")
 public class MainApplication {
